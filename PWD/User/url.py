eb24d363e45190648610ff49ac149ffb90a12b2f
NOREEN ELIZA ROY <NoreenElizaRoy>
2024-03-12 23:37:22 +0530

    feat:authentication

--- PWD/User/url.py
+++ PWD/User/url.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView,LoginView
+from .views import RegisterView,LoginView,UserView,LogoutView
 
 app_name= "User"
 urlpatterns = [
     path('register', RegisterView.as_view(),name='user-register'),
-    path('login', LoginView.as_view(),name='user-login' )
-    
+    path('login', LoginView.as_view(),name='user-login' ),
+    path('user', UserView.as_view(),name='user-view' ),
+    path('logout', LogoutView.as_view(),name='user-logout' )
 ]
